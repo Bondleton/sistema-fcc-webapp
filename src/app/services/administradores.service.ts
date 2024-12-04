937bec04aeb945f685f5b2f2a00467b6d7b1a435
Bondleton <itzelf182@gmail.com>
2024-12-04 07:41:13 -0600

    Tamaños

--- src/app/services/administradores.service.ts
+++ src/app/services/administradores.service.ts
@@ -136,7 +136,7 @@ export class AdministradoresService {
     return this.http.delete<any>(`${environment.url_api}/admins-edit/?id=${idUser}`, { headers: headers });
   }
 
-  //Obtener el total de cada uno de los usuarios 
+  //Obtener el total de cada uno de los usuarios
   public getTotalUsuarios(){
     var token = this.facadeService.getSessionToken();
     var headers = new HttpHeaders({ 'Content-Type': 'application/json' , 'Authorization': 'Bearer '+token});
